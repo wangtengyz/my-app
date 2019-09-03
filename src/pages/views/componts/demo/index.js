86fb1edf2e2812810205a4bfc8bdf892256ed796
wangteng <wangteng@inke.cn>
2019-09-03 11:04:19 +0800

    demo

---
+++ src/pages/views/componts/demo/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Hello from './index.module.less'
+// css 模块化
+export default class ManageIndex extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
